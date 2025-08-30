571751af673fa3f301f6039f79940cc9703885a5
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-30 15:38:37 +0530

    loginSeller() function added.

--- src/redux/features/sellerSlice.js
+++ src/redux/features/sellerSlice.js
@@ -1,12 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { api } from "../../api";
+
+export const loginSeller = createAsyncThunk("seller/login", async ({ email, password }, { rejectWithValue }) => {
+    try {
+        const { data } = await api.post("/seller/login", { email, password });
+        localStorage.setItem("token", data.token);
+        return data;
+    } catch (error) {
+        return rejectWithValue(error.response?.data?.message || "Login Failed");
+    }
+});
 
 const sellerSlice = createSlice({
     name: "seller",
 
-    reducers:{
+    reducers: {
 
     },
-    extraReducers:{
+    extraReducers: {
 
     }
 });
