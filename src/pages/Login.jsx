d247562c95e77bc3c087079e8f3e51979508549a
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-31 10:23:55 +0530

    login updated and react-toastify udpated.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,34 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { loginSeller } from '../redux/features/sellerSlice';
+
 
 const Login = () => {
     const { error, loading } = useSelector((state) => state.seller);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // handleSubmit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(email, password);
+        if (!email) {
+            console.log("email is required");
+            return;
+        };
+        if (!password) {
+            console.log("password is required");
+            return;
+        };
+
+        dispatch(loginSeller({email, password}));
+        console.log(email, password);
+    }
 
     return (
         <div className='auth'>
@@ -15,22 +38,25 @@ const Login = () => {
                 </div>
             </div>
             <div className="auth-form">
-                <form >
+                <form onSubmit={handleSubmit} >
                     <div className="form-header">
                         <h2>Welcome Back</h2>
                     </div>
 
                     <div className="form-body">
-                        <div className="error">
-                            <p>{error}</p>
+                        {
+                            error && <div className="error">
+                                <p>{error}</p>
+                            </div>
+                        }
+
+                        <div className="form-control">
+
+                            <input type="text" placeholder='Enter Email' value={email} onChange={(e) => { setEmail(e.target.value) }} />
                         </div>
                         <div className="form-control">
 
-                            <input type="text" placeholder='Enter Email' />
-                        </div>
-                        <div className="form-control">
-
-                            <input type="text" placeholder='Enter Password' />
+                            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter Password' />
                         </div>
                         <div className="form-actions">
                             <button>
@@ -44,7 +70,7 @@ const Login = () => {
                                 }
                             </button>
                             <p className="switchText">
-                                Create a new account <span>Register</span>
+                                Create a new account <span onClick={() => navigate("/register")}>Register</span>
                             </p>
                         </div>
                     </div>
