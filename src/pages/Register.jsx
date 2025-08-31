4440c958a0061ba6d8a1b7e656ef1d6a1fe86cc0
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-08-31 22:20:37 +0530

    register form created.

---
+++ src/pages/Register.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { registerSeller } from '../redux/features/sellerSlice';
+import { toast } from 'react-toastify';
+import "../css/auth.css"
+
+const Register = () => {
+    const { loading } = useSelector((state) => state.seller);
+    const [form, setForm] = useState({
+        name: "",
+        email:"",
+        password: "",
+        storeName: ""
+    })
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // handlechange
+    const handleChange = (e)=>{
+        const {name, value} = e.target;
+        setForm({...form, [name]:value});
+    };
+    
+    // handleSubmit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        
+        if (!form.email) {
+            // console.log("email is required");
+            toast.warn("Email is required");
+            return;
+        };
+        if (!form.password) {
+            // console.log("password is required");
+            toast.warn("Password is required");
+            return;
+        };
+        if(!form.name){
+            toast.warn("Name is required");
+            return;
+        };
+        if(!form.storeName){
+            toast.warn("Store Name is required");
+            return;
+        }
+
+        dispatch(registerSeller(form));
+        
+    }
+
+    return (
+        <div className='auth'>
+            <div className="auth-text">
+                <div className="auth-text-content">
+                    <h2>Welcome Back!</h2>
+                    <h1>Partner Panel</h1>
+                    <p>Manage your bookstore, track your sales, and reach more readers with ease.</p>
+                    <p>Log in to access your seller dashboard and continue growing your business.</p>
+                </div>
+            </div>
+            <div className="auth-form">
+                <form onSubmit={handleSubmit} >
+                    <div className="form-header">
+                        <h2>Seller Registration</h2>
+                    </div>
+
+                    <div className="form-body">
+
+
+                        <div className="form-img">
+                                <img src="https://www.aceinfoway.com/blog/wp-content/uploads/2020/06/The-Return-of-Froogle-Robinhood-of-Google-Shopping-1.gif" alt="" />
+                            </div>
+
+                        <div className="form-control">
+
+                            <input type="email" placeholder='Enter Email' value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                        </div>
+                        <div className="form-control">
+
+                            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter Password' />
+                        </div>
+                        <div className="form-actions">
+                            <button>
+                                {
+                                    loading ? (
+                                        <div className="loading-text">
+                                            <div className="spinner"></div>
+                                            <p>Logging In...</p>
+                                        </div>
+                                    ) : ("Login")
+                                }
+                            </button>
+                            <p className="switchText">
+                                Create a new account <span onClick={() => navigate("/register")}>Register</span>
+                            </p>
+                        </div>
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register
