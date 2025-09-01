82d5b035f3e3df66d650d3c525d6b2113ab82d34
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-09-01 15:11:32 +0530

    conditional routing included.

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import './App.css'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import { useSelector } from 'react-redux'
 
 function App() {
-  
+  const {token} = useSelector(state=>state.seller);
+  // console.log(token);
 
   return (
     <>
      <BrowserRouter>
      <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/' element={token ? (<Dashboard/>):(<Login/>) }/>
+      <Route path='/register' element={ token ? (<Dashboard/>) : (<Register/>)}/>
+      <Route path='/dashboard' element={token ? (<Dashboard/>):(<Login/>) }/>
      </Routes>
      </BrowserRouter>
     </>
