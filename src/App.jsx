c23b2139931c0c8a7105f2ca6fe12c7719d97e2e
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-09-02 21:16:19 +0530

    Settings page added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import { useSelector } from 'react-redux'
+import './App.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import { useSelector } from 'react-redux';
+import Profile from './pages/Profile';
+
 
 function App() {
-  const {token} = useSelector(state=>state.seller);
+  const { token } = useSelector(state => state.seller);
   // console.log(token);
 
   return (
     <>
-     <BrowserRouter>
-     <Routes>
-      <Route path='/' element={token ? (<Dashboard/>):(<Login/>) }/>
-      <Route path='/register' element={ token ? (<Dashboard/>) : (<Register/>)}/>
-      <Route path='/dashboard' element={token ? (<Dashboard/>):(<Login/>) }/>
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={token ? (<Dashboard />) : (<Login />)} />
+          <Route path='/register' element={token ? (<Dashboard />) : (<Register />)} />
+          <Route path='/dashboard' element={token ? (<Dashboard />) : (<Login />)}>
+            <Route path='profile' element={<Profile/>} />
+            <Route path='settings' element={} />
+            <Route path='' element={} />
+            <Route path='' element={} />
+            <Route path='' element={} />
+            <Route path='' element={} />
+            <Route path='' element={} />
+            <Route path='' element={} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
