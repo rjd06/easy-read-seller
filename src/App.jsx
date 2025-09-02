08a311420f0b1da5cc418c54ffb0e6ebf018fa2b
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-09-02 21:21:46 +0530

    dashboard nested pages routes added.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,14 @@ import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import { useSelector } from 'react-redux';
 import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import PublishBook from './pages/PublishBook';
+import DeleteBook from './pages/DeleteBook';
+import MyBooks from './pages/MyBooks';
+import Orders from './pages/Orders';
+import UpdateBook from './pages/UpdateBook';
+import ViewBook from './pages/ViewBook';
+import ViewOrder from './pages/ViewOrder';
 
 
 function App() {
@@ -19,14 +27,15 @@ function App() {
           <Route path='/' element={token ? (<Dashboard />) : (<Login />)} />
           <Route path='/register' element={token ? (<Dashboard />) : (<Register />)} />
           <Route path='/dashboard' element={token ? (<Dashboard />) : (<Login />)}>
-            <Route path='profile' element={<Profile/>} />
-            <Route path='settings' element={} />
-            <Route path='' element={} />
-            <Route path='' element={} />
-            <Route path='' element={} />
-            <Route path='' element={} />
-            <Route path='' element={} />
-            <Route path='' element={} />
+            <Route path='profile' element={<Profile />} />
+            <Route path='settings' element={<Settings />} />
+            <Route path='publish_book' element={<PublishBook />} />
+            <Route path='delete_book' element={<DeleteBook />} />
+            <Route path='my_books' element={<MyBooks />} />
+            <Route path='orders' element={<Orders />} />
+            <Route path='update_book' element={<UpdateBook />} />
+            <Route path='view_book' element={<ViewBook />} />
+            <Route path='view_order' element={<ViewOrder />} />
           </Route>
         </Routes>
       </BrowserRouter>
