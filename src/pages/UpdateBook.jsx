611837b841f561b7a73096de0a20ffdc2f010cbd
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-09-06 16:16:58 +0530

    updatebook handler function added.

--- src/pages/UpdateBook.jsx
+++ src/pages/UpdateBook.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
 const UpdateBook = () => {
   const [title, setTitle] = useState("");
@@ -8,6 +9,49 @@ const UpdateBook = () => {
   const [stock, setStock] = useState("");
   const [bookCover, setBookCover] = useState("");
   const [description, setDescription] = useState("");
+
+
+  // handle update form submit
+  const handleUpdateBookSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title) {
+      toast.warn("Title is required.");
+      return;
+    };
+
+    if (!category) {
+      toast.warn("Category is required.");
+      return;
+    };
+
+    if (!author) {
+      toast.warn("Author is required.");
+      return;
+    };
+
+    if (!price) {
+      toast.warn("Price is required.");
+      return;
+    };
+
+    if (!stock) {
+      toast.warn("Stock is required.");
+      return;
+    };
+
+    if(!bookCover){
+      toast.warn("Book cover is required.");
+      return;
+    };
+
+    if(!description){
+      toast.warn("Description is required.");
+      return;
+    }
+
+    console.log("Updated successfully.");
+  }
   return (
     <div>UpdateBook</div>
   )
